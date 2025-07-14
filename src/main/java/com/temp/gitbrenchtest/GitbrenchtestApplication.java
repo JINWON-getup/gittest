ab98f334185cf230fe882100c7cab81f16e22023
jin <wlsdnjs3225@naver.com>
2025-07-15 08:43:54 +0900

    init 셋팅

---
+++ src/main/java/com/temp/gitbrenchtest/GitbrenchtestApplication.java
@@ -0,0 +1,13 @@
+package com.temp.gitbrenchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbrenchtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitbrenchtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/temp/gitbrenchtest/GitbrenchtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temp.gitbrenchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbrenchtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
